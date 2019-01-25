b43fe74ae714ce4c607be62b3f80c712280e07ed
unknown <ZEAC122@live.rhul.ac.uk>
2019-01-25 15:05:51 +0000

    Added a SingleMenuCard which takes some properties and displays a card with some menu information. The current version demonstrates a test for the card. Issues are being added to detail the next steps

---
+++ frontend/src/MenuComponents/MenuCard.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {Image, Card, Icon, Button} from 'semantic-ui-react';
+
+/**
+ * This class is meant to be a container for a single menu item ie A Single <b> Dish </b>.
+ * The Card used is a part of the Semantic UI React library for UI components. 
+ * 
+ * The class takes properties and then displays those properties appropriately within the card.
+ * 
+ * The card will take an image from the properties as the "dishImage"
+ * The card header will display the "dishName"
+ * The card Description will display the "dishInfo"
+ * The card meta will display the "dishPrice"
+ * 
+ * The extra section of the Card will also display icons, one of which opens up allergy information
+ * and the other adds the dish to the basket. 
+ * 
+ * 
+ */
+class MenuItemCard extends React.Component{
+    constructor(props){
+        super(props);
+        const dishID = this.props.dishID; // dishID is a unique identifier for each dish.
+    }
+
+    render(){
+        return(
+            <React.Fragment>
+                <Card> {/*The main wrapper component for the whole card - from Library*/}
+                    <Image src={this.props.dishImage}/>
+                    <Card.Content textAlign={"center"}> {/*Wraps main body of card while "extra" tag wraps the bottom*/}
+                        <Card.Header> {this.props.dishName} </Card.Header>
+                        <Card.Description> {this.props.dishInfo} </Card.Description>
+                        <Card.Meta> {this.props.dishPrice} </Card.Meta>
+                    </Card.Content>
+
+                    <Card.Content extra={true}>
+                        <Button icon={true}> {/* Button for adding the item to basket */}
+                            <Icon name="plus"/>
+                        </Button>
+
+                        <Button icon={true}>
+                            <Icon name="info"></Icon>
+                        </Button>
+
+                    </Card.Content>
+
+                </Card>
+            </React.Fragment>
+        )
+    }
+}
+
+export default MenuItemCard; // This is so that the rest of the program can access the MenuItemCard using: import <name> from MenuCard.js
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,18 @@
+/**
+ * The class which is loaded first by react and will be used to put together everything. For now, this class is used
+ * to render some of the components while the rest is made to test those components. 
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import DishCardTest from './MenuComponents/MenuCard.js';
 import './index.css';
+
+
+ReactDOM.render(
+    <DishCardTest dishName="Dish Name Test" dishInfo="This is some information to test the information"
+    dishPrice="£Test" dishImage="https://images-gmi-pmc.edge-generalmills.com/0c0d3f02-0bc0-4ca3-ae87-fedb93b745a3.jpg"
+    dishId="TestAB123"/>,
+
+    document.getElementById("SingleMenuCardTest")
+); 
