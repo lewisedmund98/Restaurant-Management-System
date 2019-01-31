c9fceab39fff9f269665aa60a37cba2c9415ccb3
Hugh <hugh@crablab.co.uk>
2019-01-31 22:42:01 +0000

    Change to new v8 database

--- backend/frameworks/database/db.py
+++ backend/frameworks/database/db.py
@@ -2,7 +2,7 @@ import pymysql
 
 class db():
     def __init__(self):
-        self.__db = pymysql.connect("138.68.169.22","teamproject","a6ErM22GHr36CUHF","teamproject", cursorclass=pymysql.cursors.DictCursor)
+        self.__db = pymysql.connect("178.62.61.46","teamproject","a6ErM22GHr36CUHF","teamproject", cursorclass=pymysql.cursors.DictCursor)
 
     def getInstance(self):
         return self.__db
