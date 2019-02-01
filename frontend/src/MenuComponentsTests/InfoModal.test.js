5628426c9696b992c9d0f077eb1a5a7c13d664cd
HarryChurchill <zdac071@live.rhul.ac.uk>
2019-02-01 04:32:13 +0000

    Added some tests for the infoModal

---
+++ frontend/src/MenuComponentsTests/InfoModal.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import InfoModal from '../MenuComponents/InfoModal';
+import { shallow } from 'enzyme';
+
+describe('<InfoModal /', () => {
+	const infoDetails = {
+		allergy: ["nuts"]
+	}
+	
+	const noDetails ={
+		allergy: []
+	}
+	
+	const componentA = shallow(<InfoModal mapAllergyInformation = {infoDetails}/>);
+	
+	const componentB = shallow(<InfoModal mapAllergyInformation = {noDetails}/>);
+	
+	it('Displays list of allergies', () => {
+		expect(componentA.contains(<li>nuts</li>)).toBe(true);
+	});
+	it('Displays no allergies message', () => {
+		expect(componentB.contains(<li>There are no allergies to display</li>)).toBe(true);
+	});
+	
+})
