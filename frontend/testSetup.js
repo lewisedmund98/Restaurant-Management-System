3ab03d137e94fc45a2eec4db1cd35b0a30c8d545
HarryChurchill <zdac071@live.rhul.ac.uk>
2019-02-04 21:58:52 +0000

    Added changes to the json and testsetup to try and allow the tests to run

--- frontend/testSetup.js
+++ frontend/testSetup.js
@@ -1,5 +1,8 @@
+//import { configure } from 'enzyme';
+//import Adapter from 'enzyme-adapter-react-16';
 import * as enzyme from 'enzyme';
 import * as Adapter from 'enzyme-adapter-react-16';
 
+//configure({ adapter: new adapter()});
 
 enzyme.configure({ adapter: new Adapter() });
