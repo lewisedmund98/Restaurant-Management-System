1f7b9961c1ad00bca2d5c829c84224fd4e95ac0a
Hugh <hugh@crablab.co.uk>
2019-02-05 19:36:59 +0000

    Comments

--- backend/frameworks/authentication/auth.py
+++ backend/frameworks/authentication/auth.py
@@ -1,12 +1,17 @@
 from frameworks.database.db import db
 from argon2 import PasswordHasher
 
+# Important to note that there aren't inherant security features built into 
+# this class as it is designed to be called by API handlers. Python also doesn't have
+# private methods so there isn't much point designing code to prevent access tokens
+# being generate out of sequence as the method can be called anyway 
 class authentication():
     def __init__(self):
         instance = db()
         self.__db = instance.getInstance()
         self.__ph = PasswordHasher()
 
+    # Public. Authenticates user against DB and returns bool
     def authenticateUser(self, username, password):
         output = self.__getUser(username)
         try:
@@ -23,3 +28,7 @@ class authentication():
         cursor = self.__db.cursor()
         cursor.execute("SELECT * FROM `users` WHERE `username` = %s LIMIT 1;", (user))
         return cursor.fetchone()
+
+    def __generateAccessToken(self, secret, uid):
+        
+        return
