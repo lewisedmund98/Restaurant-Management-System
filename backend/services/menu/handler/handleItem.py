4afe251980336a585d6a03a856ab60243513d8a3
Joe <zeac125@live.rhul.ac.uk>
2019-02-06 13:56:04 +0000

    adding item frameworks and refactoring handleItem to allow abstract database calls

--- backend/services/menu/handler/handleItem.py
+++ backend/services/menu/handler/handleItem.py
@@ -1,15 +1,21 @@
 import pymysql
 from frameworks.database.db import db
+from frameworks.item.item import item
 
 class handleItem:
     def __init__(self, request):
         instance = db()
         self.__db = instance.getInstance()
         self.__data = request
+        self.__item = item() 
 
     def getOutput(self):
-        return {"result": self.printMenu()}
+        return {"result": self.getItem()}
 
+    def getItem(self):
+        print(self.__data['id'])
+        item.load(self.__data['id'])
+        return item.data 
 
     def printMenu(self):
         if(self.__data['id']):
