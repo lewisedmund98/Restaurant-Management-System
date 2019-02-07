f3549134185b888de0c5f9679d4ae897ef06e30a
Hugh <hugh@crablab.co.uk>
2019-02-07 23:51:07 +0000

    Changed wording to be clear

--- backend/services/authentication/handler/handleLogin.py
+++ backend/services/authentication/handler/handleLogin.py
@@ -10,8 +10,8 @@ class handleLogin:
         return {"login": self.__login()}
 
     def __login(self):
-        uid = self.__auth.authenticateUser(self.__data['access_token'], self.__data['token_secret'], self.__data['username'], self.__data['password'])
+        uid = self.__auth.authenticateUser(self.__data['key'], self.__data['secret'], self.__data['username'], self.__data['password'])
         if(uid == False):
             return False
-        at = self.__auth.generateAccessToken(self.__data['access_token'], self.__data['token_secret'], uid)
+        at = self.__auth.generateAccessToken(self.__data['key'], self.__data['secret'], uid)
         return {"access_token": at['access_token'], "userID": uid}
