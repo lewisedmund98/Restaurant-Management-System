ea321f0a7e317c98c3b7f5b69ecb969eab65b884
Hugh <hugh@crablab.co.uk>
2019-02-08 10:01:36 +0000

    Refactor authentication class and tests

--- backend/backend_tests/frameworks.authentication/test_authentication.py
+++ backend/backend_tests/frameworks.authentication/test_authentication.py
@@ -3,27 +3,24 @@ import pytest
 @pytest.fixture
 def authClass():
     from frameworks.authentication.auth import authentication
-    return authentication()
+    return authentication("abc123", "def456")
 
 def testauthTrue(authClass):
-    assert isinstance(authClass.authenticateUser("abc123", "def456", "test", "s3kr3tp4ssw0rd"), str)
+    assert isinstance(authClass.authenticateUser("test", "s3kr3tp4ssw0rd"), str)
 
 def testauthFalse(authClass):
-    assert authClass.authenticateUser("abc123", "def456", "test", "r@nd0m") == False
-
-def testAuthGenAccessTokFalse(authClass):
-    assert authClass.generateAccessToken("random", "random", "asdasd123123") == False
+    assert authClass.authenticateUser("test", "r@nd0m") == False
 
 def testAuthGenAccessTokTrue(authClass):
-    assert isinstance(authClass.generateAccessToken("abc123", "def456", "asdasd123123"), dict)
+    assert isinstance(authClass.generateAccessToken("asdasd123123"), dict)
 
 def testAuthRequestNoUser(authClass):
-    assert authClass.authenticateRequest("abc123", "def456", "n/a" , "random", 0) == 404
+    assert authClass.authenticateRequest("n/a" , "random", 0) == 404
 
 def testAuthRequestUserWrongLevel(authClass):
-    assert authClass.authenticateRequest("abc123", "def456", "n/a", "asdasd123123", -1) == 403
+    assert authClass.authenticateRequest("n/a", "asdasd123123", -1) == 403
 
 def testAuthFullFlow(authClass):
-    assert isinstance(authClass.authenticateUser("abc123", "def456", "test", "s3kr3tp4ssw0rd"), str)
-    token = authClass.generateAccessToken("abc123", "def456", "asdasd123123")
-    assert isinstance(authClass.authenticateRequest("abc123", "def456", token['access_token'], "asdasd123123", 0), dict)
+    assert isinstance(authClass.authenticateUser("test", "s3kr3tp4ssw0rd"), str)
+    token = authClass.generateAccessToken("asdasd123123")
+    assert isinstance(authClass.authenticateRequest(token['access_token'], "asdasd123123", 0), dict)
