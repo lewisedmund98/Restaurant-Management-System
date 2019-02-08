8976402c9206812072da0e49a1902403a400e9d0
Joe <zeac125@live.rhul.ac.uk>
2019-02-08 10:32:50 +0000

    Fix merge conflict

--- backend/services/menu/handler/handleItem.py
+++ backend/services/menu/handler/handleItem.py
@@ -10,17 +10,5 @@ class handleItem:
         return {"result": self.getItem()}
 
     def getItem(self):
-<<<<<<< HEAD
-        print(self.__data['id'])
-        item.load(self, self.__data['id'])
-        return item.data 
-
-    def printMenu(self):
-        if(self.__data['id']):
-            cursor = self.__db.cursor()
-            cursor.execute("SELECT * FROM teamproject.menuItems WHERE teamproject.menuItems.itemID = %s;", (self.__data['id']))
-            return cursor.fetchall()
-=======
         self.__item.load(self.__data.get_json()['id'])
-        return self.__item.get()
->>>>>>> eb69609d95b696136af1c885d07d8f0065739ec5
+        return self.__item.get()w
