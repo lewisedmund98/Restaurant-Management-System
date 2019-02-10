0fba09429b9fe90480f9361a609ae2337b532f4d
WilliamBr25 <ZEAC039@live.rhul.ac.uk>
2019-02-10 20:08:41 +0000

    added single rendered checkbox, and export

--- frontend/src/MenuComponents/MenuFiltering.js
+++ frontend/src/MenuComponents/MenuFiltering.js
@@ -1,2 +1,14 @@
 import React from 'react';
-import TabWrapper from '../Controllers/CardController.js';
+import CardController from '../Controllers/CardController.js';
+import { Checkbox } from 'semantic-ui-react'
+
+
+class MenuFiltering extends React.Component {
+    render() {
+        return (
+            <Checkbox className="priceCheck" label="Price under £20"/>
+        )
+    }
+}
+
+export default MenuFiltering;
