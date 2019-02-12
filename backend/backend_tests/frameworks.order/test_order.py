6d22a4e01d1fb191ab6a984afb78c5e2e38aafe1
Hugh <hugh@crablab.co.uk>
2019-02-12 12:54:42 +0000

    Remove stupid decorators that break the CI

--- backend/backend_tests/frameworks.order/test_order.py
+++ backend/backend_tests/frameworks.order/test_order.py
@@ -7,28 +7,18 @@ from frameworks.order.order import order
 def orderClass():  # does class exist?
     return order()
 
-
-# def testConstructor():  # does constructor work?
-#    assert order("aaa", "aaa", "aaa", "aaa", "aaa", "aaa", )
-
-@staticmethod
 def testID():
     print(order.returnID()) # does the ID generation method work?
     assert isinstance(order.returnID(), str)
 
-
-@staticmethod
 def testGetOrderInfo():  # does the getOrderInfo method exist?
     assert order.getOrderInfo()
 
-@staticmethod
 def testGetOrderStatus():  # does the getOrderStatus method exist?
     assert order.getOrderStatus()
 
-@staticmethod
 def testLoadStatusOfOrder():  # does the loadStatusOfOrder method exist?
     assert order.loadStatusOfOrder("AHHHHHHH")
 
-@staticmethod
 def testLoadInfoOfOrder():  # does the loadInformationOfOrder method exist?
     assert order.loadInformationOfOrder("AHHHHHHH")
