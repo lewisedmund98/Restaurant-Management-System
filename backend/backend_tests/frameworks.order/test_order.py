4d2d70bd1fe976eca0174422e90d8da81da6b957
T_J <ca461f40@opayq.com>
2019-02-12 07:37:43 +0000

    more tests and order class stuff

--- backend/backend_tests/frameworks.order/test_order.py
+++ backend/backend_tests/frameworks.order/test_order.py
@@ -8,18 +8,23 @@ def orderClass():  # does class exist?
     return order()
 
 
-#def testConstructor():  # does constructor work?
+# def testConstructor():  # does constructor work?
 #    assert order("aaa", "aaa", "aaa", "aaa", "aaa", "aaa", )
 
 
-#def testConstructor():  # does constructor work?
+# def testConstructor():  # does constructor work?
 #    assert order("aaa", "aaa", "aaa", "aaa", "aaa", "aaa", )
 
+@staticmethod
+def testID():
+    print(order.returnID()) # does the ID generation method work?
+    assert isinstance(order.returnID(), str)
 
-#def testID():
-#    print(order.returnID())
-#    assert isinstance(order.returnID(), str)
 
-
-def testgetData():
+@staticmethod
+def testGetData(): # does the getData method exist?
     assert order.getData()
+
+@staticmethod
+def testGetStatus():  # does the getStatus method exist?
+    assert order.getStatus()
--- backend/frameworks/order/order.py
+++ backend/frameworks/order/order.py
@@ -2,29 +2,27 @@ from frameworks.database.db import db
 from frameworks.idGenerator.id import id
 
 
+# noinspection PyPep8Naming
 class order():
 
+    # noinspection PyUnusedLocal
     def __init__(self):
         instance = db()
-        self.__id = id() # id
-        self.__data = "" # order information
-        self.__status = "" # order status
+        self.__id = id()  # id
+        self.__data = ""  # order information
+        self.__status = ""  # order status
 
-
-    def returnID():
+    # noinspection PyMethodMayBeStatic
+    def returnID(self):
         return id.getID("order")
-
-
 
         # sql stuff to add order to database, use ID generator to make an order ID
 
-
-
-    def loadOrderInformation(data):
-        data = data
-
-
-
+    def loadOrderInformation(self, data):
+        self.__data = data
 
     def getData(self):
         return self.__data
+
+    def getStatus(self):
+        return self.__status
