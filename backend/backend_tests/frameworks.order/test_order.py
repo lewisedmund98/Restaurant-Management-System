5ace7cb9b2184de5213f48553c34441509b8f0c9
Hugh <hugh@crablab.co.uk>
2019-02-12 15:49:47 +0000

    Fix broken tests

--- backend/backend_tests/frameworks.order/test_order.py
+++ backend/backend_tests/frameworks.order/test_order.py
@@ -7,14 +7,10 @@ from frameworks.order.order import order
 def orderClass():  # does class exist?
     return order()
 
-def testGetOrderInfo(orderClass):  # does the getOrderInfo method exist?
-    assert isinstance(orderClass.getOrderInfo(), dict)
-
-def testGetOrderStatus(orderClass):  # does the getOrderStatus method exist?
+def testLoadStatusOfOrder(orderClass):  # does the loadStatusOfOrder method exist?
+    assert orderClass.loadOrderStatus("ordercjs1sk6zd0000adaxgzznu4dh") == True
     assert isinstance(orderClass.getOrderStatus(), dict)
 
-def testLoadStatusOfOrder(orderClass):  # does the loadStatusOfOrder method exist?
-    assert orderClass.loadStatusOfOrder("ordercjs1sk6zd0000adaxgzznu4dh") == True
-
-def testLoadInfoOfOrder(orderClass):  # does the loadInformationOfOrder method exist?
+def loadOrderInfo(orderClass):  # does the loadInformationOfOrder method exist?
     assert orderClass.loadInformationOfOrder("ordercjs1sk6zd0000adaxgzznu4dh") == True
+    assert isinstance(orderClass.getOrderInfo(), dict)
