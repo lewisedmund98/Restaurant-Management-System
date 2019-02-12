b4e409b6523792b9c7a4c4eda64fe358b78578be
WilliamBr25 <ZEAC039@live.rhul.ac.uk>
2019-02-12 12:01:34 +0000

    Remove method added

--- frontend/src/MenuComponents/MenuFiltering.js
+++ frontend/src/MenuComponents/MenuFiltering.js
@@ -12,8 +12,20 @@ class MenuFiltering extends React.Component {
     }
 
     filter() {
+        //var tempDishList = this.prop.dishList;
+        //var tempMenuItem = tempDishList[tempDishList.length - 1];
+    }
+
+    remove(menuItem) { //call this method with the menuItem that needs to be removed (from filter) and it will be removed
+        // just putting this here to simplify my code and make a start
         var tempDishList = this.prop.dishList;
         var tempMenuItem = tempDishList[tempDishList.length - 1];
+        tempDishList[tempDishList.length - 1] = menuItem;
+        tempDishList[tempDishList.indexOf(menuItem)] = tempMenuItem; // simple three way swap with a temp variable
+        tempDishList.pop(); //removes the unwanted menuItem
+        this.setState({
+            dishList: tempDishList,
+        })
     }
 
     render() {
