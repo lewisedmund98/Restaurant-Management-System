f411051d95c4db7a4afc9b43882968ae9e2ee895
Hugh <hugh@crablab.co.uk>
2019-02-12 20:46:52 +0000

    Wrote orders class and tests

---
+++ backend/backend_tests/frameworks.order/test_orders.py
@@ -0,0 +1,12 @@
+import pytest
+# noinspection PyUnresolvedReferences
+from frameworks.order.orders import orders
+
+
+@pytest.fixture
+def ordersClass():  
+    return orders()
+
+def testOrders(ordersClass):
+    assert ordersClass.loadOrders() == True
+    assert isinstance(ordersClass.getOrders(), list)
