dc7b5e88df75a299002aec382538ed01026feb6a
Hugh <hugh@crablab.co.uk>
2019-02-14 19:55:24 +0000

    Trying out a fix

--- backend/frameworks/order/staffOrder.py
+++ backend/frameworks/order/staffOrder.py
@@ -5,6 +5,7 @@ from frameworks.idGenerator.id import id
 
 class staffOrder(order):
     def __init__(self, orderID):
+        super(staffOrder, self).__init__()
         instance = db()
         self.__db = instance.getInstance()
         self.__id = orderID
@@ -13,12 +14,12 @@ class staffOrder(order):
         order = self.__id
         stage = "waiterConfirmed"
         meta = {}
-        iID = super(order, self).__insertOrderHistory(order, stage, meta)
+        iID = self.__insertOrderHistory(order, stage, meta)
         return iID
 
     def kitchenCorfirm(self, ETA):
         meta = {'deliverTime' : ETA}
         order = self.__id
         stage = "kitchenCorfirm"
-        iID =super(order,self).__insertOrderHistory(order, stage, meta)
+        iID = self.__insertOrderHistory(order, stage, meta)
         return iID
