234d2d5520219ca88a2221fb633daafeb2026660
WilliamBr25 <ZEAC039@live.rhul.ac.uk>
2019-02-19 23:08:29 +0000

    OrderForm styling, buttons have been made nice, some shifting around of buttons with a margin to create nicer spacing, text edited to be more in line with the style of the website. Feedback welcome

--- frontend/src/BasketComponents/OrderForm.js
+++ frontend/src/BasketComponents/OrderForm.js
@@ -84,20 +84,20 @@ export default class OrderForm extends React.Component {
         return (
             <Form onSubmit={this.createOrder}>
                 <Form.Field>
-                    <Label>Full Name</Label>
-                    <Input required onChange={this.handleInputChange} name="name" placeholder='Full Name' />
+                    <Label class="basketLabel">Full Name</Label>
+                    <Input required onChange={this.handleInputChange} name="name" placeholder='Full Name...' />
                 </Form.Field>
                 
                 <Form.Field>
-                    <Label>Email Address</Label>
-                    <Input required onChange={this.handleInputChange} name="email" placeholder='Email Address' />
+                    <Label class="basketLabel">Email Address</Label>
+                    <Input required onChange={this.handleInputChange} name="email" placeholder='Email Address...' />
                 </Form.Field>
                 <Form.Field>
-                    <Label>Phone Number</Label>
-                    <Input required onChange={this.handleInputChange} name="phone" placeholder='Phone Number' />
+                    <Label class="basketLabel">Phone Number</Label>
+                    <Input required onChange={this.handleInputChange} name="phone" placeholder='Phone Number...' />
                 </Form.Field>
                 <Form.Field>
-                    <Label>Table Number</Label>
+                    <Label class="basketLabel">Table Number</Label>
                     <Dropdown required onChange={this.handleInputChange} name="tableNumber"
                         placeholder='Select a table...' search selection options={this.tables} />
 
