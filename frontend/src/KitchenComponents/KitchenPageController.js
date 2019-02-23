efb1cb8dc7afcc7db305f5909607b9e9d295e1fb
WilliamBr25 <ZEAC039@live.rhul.ac.uk>
2019-02-23 17:18:18 +0000

    beginning to add the OrderCard to the kitchen page, need to figure out why it isn't rendering

--- frontend/src/KitchenComponents/KitchenPageController.js
+++ frontend/src/KitchenComponents/KitchenPageController.js
@@ -2,5 +2,23 @@ import React from 'react';
 import KitchenPageWrapper from "./KitchenPageWrapper";
 
 export default class KitchenPageController extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            unconfirmedOrders: [],
+            toBeDelivered: [],
+            twentyFourHours: [],
+            accessToken: this.props.accessToken
+        };
 
+    }
+
+    render() {
+        if (this.props.accessToken) {
+            console.log(this.props.accessToken);
+        }
+        return (
+            <KitchenPageWrapper/>
+        )
+    }
 }
--- frontend/src/KitchenComponents/KitchenPageWrapper.js
+++ frontend/src/KitchenComponents/KitchenPageWrapper.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import KitchenItemsView from "./KitchenItemsView";
+import { Card, Button } from 'semantic-ui-react';
 
 export default class KitchenPageWrapper extends React.Component {
-
+    render() {
+        return (
+            <React.fragment>
+                <Card className="menuCard">
+                    <Card.content>
+                        <KitchenItemsView unconfirmed={true} />
+                    </Card.content>
+                    <Card.Content extra={true}>
+                        <Button>+5</Button>
+                    </Card.Content>
+                </Card>
+            </React.fragment>
+        )
+    }
 }
