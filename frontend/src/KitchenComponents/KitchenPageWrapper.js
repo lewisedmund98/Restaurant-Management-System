741b72265989e5c2a3f05962ade05e0db02e3179
WilliamBr25 <ZEAC039@live.rhul.ac.uk>
2019-02-23 17:24:01 +0000

    Added h1 page title, mostly commiting so i can sync merge

--- frontend/src/KitchenComponents/KitchenPageWrapper.js
+++ frontend/src/KitchenComponents/KitchenPageWrapper.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import KitchenItemsView from "./KitchenItemsView";
-import { Card, Button } from 'semantic-ui-react';
+import {Card, Button} from 'semantic-ui-react';
 
 export default class KitchenPageWrapper extends React.Component {
     render() {
         return (
             <React.fragment>
-                <Card className="menuCard">
-                    <Card.content>
-                        <KitchenItemsView unconfirmed={true} />
-                    </Card.content>
-                    <Card.Content extra={true}>
-                        <Button>+5</Button>
-                    </Card.Content>
-                </Card>
+                <h1>Incoming Order</h1>
+                <div style={{padding: "20px"}} className="kitchenOrderDiv">
+                    <Card className="kitchenOrderCard">
+                        <Card.content>
+                            <Button>+5</Button>
+                            <KitchenItemsView unconfirmed={true}/>
+                        </Card.content>
+                        <Card.Content extra>
+                            <Button>+5</Button>
+                            <Button>+10</Button>
+                            <Button>+15</Button>
+                            <Button>+20</Button>
+                            <Button>+25</Button>
+                            <Button>+30</Button>
+                        </Card.Content>
+                    </Card>
+                </div>
             </React.fragment>
         )
     }
