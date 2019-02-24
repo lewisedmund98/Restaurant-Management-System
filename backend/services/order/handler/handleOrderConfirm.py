21465f7572f2a1445402b875ac68b9d9167f74cf
Joe <zeac125@live.rhul.ac.uk>
2019-02-24 16:01:53 +0000

    fixed imports

---
+++ backend/services/order/handler/handleOrderConfirm.py
@@ -0,0 +1,14 @@
+from flask import Request, abort
+from frameworks.order.orders import orders
+
+class handleOrderConfirm:
+    def __init__(self, request):
+        self.__order = order()
+        self.__data = request.get_json()
+
+    def getOutput(self):
+        return {"order has been confirmed by the waiter"  : self.__waiterConfirmation()}
+
+    def __waiterConfirmation():
+        self.order = loadOrderHistory(self.__data['id'], "waiterConfirmed")
+        return self.__order.__getWaiterConfirmed()
