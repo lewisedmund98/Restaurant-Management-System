4d9b1de7aad445ab0242fa97ae67209b892a3db5
Hugh <hugh@crablab.co.uk>
2019-03-07 10:23:51 +0000

    Add internal class

---
+++ backend/frameworks/notifications/notifications.py
@@ -0,0 +1,38 @@
+from frameworks.database.db import db
+from frameworks.idGenerator.id import id
+from datetime import datetime, timedelta
+import json
+
+class notifications:
+
+    def __init__(self):
+        # Instatiate Database
+        self.__database = instance = db()
+        self.__db = instance.getInstance()
+        # Instantiate ID
+        self.__id = id()
+    
+    def createNotification(self, table, type, meta):
+        cursor = self.__db.cursor()
+        nID = self.__id.getID("notification_")
+        cursor.execute("INSERT INTO `notifications` (`id`, `table`, `inserted`, `retrieved`, `type`, `meta`) VALUES (%s, %s, %s, null, %s, %s);", (nID, table, int(datetime.now().timestamp()), type, meta))
+        return nID
+
+    def getNotification(self, id):
+        cursor = self.__db.cursor()
+        cursor.execute("SELECT * FROM `notifications` WHERE `id` = %s", (id))
+        return cursor.fetchone()
+    
+    def getUnreadByTable(self, table):
+        cursor = self.__db.cursor()
+        cursor.execute("SELECT * FROM `notifications` WHERE `table` = %s AND `retrieved` IS NULL;", (table))
+        data = cursor.fetchall()
+        
+        for record in data:
+            self.__markRead(record['id'])
+        return data
+
+    def __markRead(self, id):
+        cursor = self.__db.cursor()
+        cursor.execute("UPDATE `notifications` SET `retrieved` = %s WHERE `id` = %s;", (int(datetime.now().timestamp()), id))
+        return id
