6fcfbe9a402c46a690a48f365c31820a435e7e5d
Haris Rabbani <ZEAC122@live.rhul.ac.uk>
2019-03-19 16:42:34 +0000

    Added the UI template for changing what menu items are currently active, currently the wrong way round

--- frontend/src/WaiterComponents/EditMenu.js
+++ frontend/src/WaiterComponents/EditMenu.js
@@ -1,87 +1,154 @@
 import React from 'react';
-import {Button, Modal} from 'semantic-ui-react';
+import { Button, Modal, List } from 'semantic-ui-react';
+import EditableMenuItem from './EditableMenuItem';
 
-export default class EditMenu extends React.Component{
-    constructor(props){
+export default class EditMenu extends React.Component {
+    constructor(props) {
         super(props);
-        this.state={
-            enabledOrders : [],
-            disabledOrders : [],
+        this.state = {
+            enabledItems: [],
+            disabledItems: [],
+            finalEnabled: [],
+            finalDisabled: []
         }
-        this.pullOrders = this.pullOrders.bind(this);
-        this.pullDisabledOrders = this.pullDisabledOrders.bind(this);
-        this.pullEnabledOrders = this.pullEnabledOrders.bind(this);
-        this.updateOrders = this.updateOrders.bind(this);
-        this.enabledOrders = this.enabledOrders.bind(this);
-        this.disabledOrders = this.disabledOrders.bind(this);
+        this.pullItems = this.pullItems.bind(this);
+        this.pullDisabledItems = this.pullDisabledItems.bind(this);
+        this.pullEnabledItems = this.pullEnabledItems.bind(this);
+        this.updateItems = this.updateItems.bind(this);
+        this.mapEnabledItems = this.mapEnabledItems.bind(this);
+        this.mapDisabledItems = this.mapDisabledItems.bind(this);
+        this.addToArray = this.addToArray.bind(this);
+        this.removeFromArray = this.removeFromArray.bind(this);
     }
 
-    componentDidMount(){
-        this.pullOrders();
+    componentDidMount() {
+        this.pullItems();
     }
 
-    async pullOrders(){
-        await this.pullDisabledOrders();
-        await this.pullEnabledOrders();
+    async pullItems() {
+        await this.pullDisabledItems();
+        await this.pullEnabledItems();
     }
 
-    async pullDisabledOrders(){
-        console.log("Pulling the disabled orders");
-        await fetch("https://flask.team-project.crablab.co/menu/items/disabled",{
+    async pullDisabledItems() {
+        await fetch("https://flask.team-project.crablab.co/menu/items/disabled", {
             headers: {
                 "Content-Type": "application/json",
             },
             method: "POST",
-            body: JSON.stringify({key: "abc123", secret: "def456"}), // pulls the order id from the order ID given
+            body: JSON.stringify({ key: "abc123", secret: "def456" }), // pulls the order id from the order ID given
         })
-        .then(response => response.json())
-        .then(disabledOrders => {
-            console.log(disabledOrders);
-        })
+            .then(response => response.json())
+            .then(disabledItems => {
+                this.setState({
+                    disabledItems: disabledItems.result
+                })
+            })
     }
 
-    async pullEnabledOrders(){
-        console.log("Pulling the enabled orders");
-        await fetch("https://flask.team-project.crablab.co/menu/items/enabled",{
+    async pullEnabledItems() {
+        await fetch("https://flask.team-project.crablab.co/menu/items/enabled", {
             headers: {
                 "Content-Type": "application/json",
             },
             method: "POST",
-            body: JSON.stringify({key: "abc123", secret: "def456"}), // pulls the order id from the order ID given
+            body: JSON.stringify({ key: "abc123", secret: "def456" }), // pulls the order id from the order ID given
         })
-        .then(response => response.json())
-        .then(enabledOrders => {
-            console.log(enabledOrders);
+            .then(response => response.json())
+            .then(enabledItems => {
+                console.log(enabledItems)
+                this.setState({
+                    enabledItems: enabledItems.result
+                })
+            })
+    }
+
+    async updateItems() {
+        console.log("updating Items");
+    }
+
+    mapEnabledItems(enabledItems) {
+        var mappedEnabled = enabledItems.map((item) => {
+            return (
+                <EditableMenuItem
+                    item={item}
+                    addToArray={this.addToArray} removeFromArray={this.removeFromArray}
+                    enable={true} itemID={item.itemID} dishName={item.itemName} itemImage={item.itemImage}></EditableMenuItem>
+            )
         })
+        return mappedEnabled;
     }
 
-    async updateOrders(){
-        console.log("updating orders");
+    mapDisabledItems(disabledItems) {
+        var mappedDisabled = disabledItems.map((item) => {
+            return (
+                <EditableMenuItem
+                    item={item}
+                    addToArray={this.addToArray} removeFromArray={this.removeFromArray}
+                    disable={true} itemID={item.itemID} dishName={item.itemName} itemImage={item.itemImage}></EditableMenuItem>
+            )
+        })
+        return mappedDisabled;
     }
 
-    mapEnabledOrders(enabledOrders){
-
-    }
-
-    mapDisabledOrders(disabledOrders){
-
-    }
-
-    render(){
-        var enabledOrdersMapped;
-        var disabledOrdersMapped;
-        if(this.state.enabledOrders){
-            enabledOrdersMapped = this.mapEnabledOrders(this.state.enabledOrders);
+    addToArray(itemID, isEnable) {
+        if (isEnable) {
+            var enabledTemp = this.state.finalEnabled;
+            enabledTemp.push(itemID);
+            this.setState({
+                finalEnabled: enabledTemp
+            })
+        } else {
+            var disabledTemp = this.state.finalEnabled;
+            disabledTemp.push(itemID);
+            this.setState({
+                finalDisabled: disabledTemp
+            })
         }
-        if(this.state.disabledOrders){
-            disabledOrdersMapped = .this.mapDisabledOrders(this.state.disabledOrders);
+    }
+
+    removeFromArray(itemID, item, isEnable) {
+        if (isEnable) {
+            var enabledTemp = this.state.finalEnabled;
+            var index = enabledTemp.indexOf(itemID);
+            enabledTemp.splice(index, 1);
+            this.setState({
+                finalEnabled: enabledTemp
+            })
+        } else {
+            var disabledTemp = this.state.finalEnabled;
+            var index = disabledTemp.indexOf(itemID);
+            disabledTemp.splice(index, 1);
+            this.setState({
+                finalDisabled: disabledTemp
+            })
         }
-        return(
+    }
+
+    render() {
+        var enabledItemsMapped;
+        var disabledItemsMapped;
+        if (this.state.enabledItems) {
+            enabledItemsMapped = this.mapEnabledItems(this.state.enabledItems);
+        }
+        if (this.state.disabledItems) {
+            disabledItemsMapped = this.mapDisabledItems(this.state.disabledItems);
+        }
+        return (
             <div>
-            <Modal trigger={<Button>Enable Menu Items</Button>}>
-            </Modal>
-            <Modal trigger={<Button>Diable Menu Items</Button>}>
-            </Modal>
+                <Modal trigger={<Button>Enable Menu Items</Button>}>
+                    <Modal.Content>
+                        <List divided relaxed>
+                            {enabledItemsMapped}
+                        </List>
+
+                    </Modal.Content>
+                </Modal>
+                <Modal trigger={<Button>Diable Menu Items</Button>}>
+                    <Modal.Content>
+                        {disabledItemsMapped}
+                    </Modal.Content>
+                </Modal>
             </div>
         )
     }
--- frontend/src/WaiterComponents/EditableMenuItem.js
+++ frontend/src/WaiterComponents/EditableMenuItem.js
@@ -1,2 +1,44 @@
 import React from 'react';
-import {Button} from 'semantic-ui-react';
+import { Button, List, Image } from 'semantic-ui-react';
+
+export default class EditableMenuItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            clicked: false
+        }
+        this.updateArray = this.updateArray.bind(this);
+    }
+
+    updateArray(itemID){
+        if(this.props.enable === true){
+            if(this.state.clicked === true){
+                this.props.addToArray(itemID, true);
+            } else {
+                this.props.removeFromArray(itemID, this.props.item, true);
+            } 
+        }
+
+        if(this.props.disable === true){
+            if(this.state.clicked === true){
+                this.props.addToArray(itemID, false);
+            } else {
+                this.props.removeFromArray(itemID, false);
+            } 
+        }
+    }
+
+    render() {
+        return (
+            <List.Item onClick={async () => {
+                await this.setState({ clicked: !this.state.clicked });
+                this.updateArray(this.props.itemID);
+            }}>
+                <List.Content style={{ backgroundColor: this.state.clicked ? "lime" : "transparent", padding: "20px" }}>
+                    <Image avatar src={this.props.itemImage}></Image>
+                    <h4>{this.props.dishName}</h4>
+                </List.Content>
+            </List.Item>
+        )
+    }
+} 
