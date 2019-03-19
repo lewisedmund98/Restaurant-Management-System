347f801ee1e016874c6691a178445f82dfa03639
Hugh <hugh@crablab.co.uk>
2019-03-19 13:17:21 +0000

    Updated menu class to support authentication

--- backend/services/menu/menu.py
+++ backend/services/menu/menu.py
@@ -4,10 +4,14 @@ from .handler.handleMenuEnabled import handleMenuEnabled
 from .handler.handleMenuDisabled import handleMenuDisabled
 from .handler.handleItem import handleItem
 from. handler.handleMenuItemsToggle import handleMenuItemsToggle
+from frameworks.authentication.auth import authentication
 
 class menu:
 
     def __init__(self, request):
+        self.__auth = authentication(request.get_json()['key'], request.get_json()['secret'])
+        self.__newAccessToken = None
+
         if(request.path == "/menu/items"):
             self.responseObj = handleMenu()
         elif(request.path == "/menu/item"):
@@ -22,7 +26,17 @@ class menu:
             self.responseObj = self
 
     def getResponse(self):
-        return self.responseObj.getOutput()
+        output = self.responseObj.getOutput()
+        if(isinstance(self.__newAccessToken, dict)):
+            output.update(self.__newAccessToken)
+        return output
 
     def getOutput(self):
         abort(404)
+
+    def __checkPermish(self, level):
+        self.__newAccessToken = self.__auth.authenticateRequest(self.__request.get_json()['access_token'], self.__request.get_json()['id'], level)
+        if(isinstance(self.__newAccessToken, dict)):
+            return True
+        else:
+            abort(403)
