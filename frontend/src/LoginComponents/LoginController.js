fab33dfb6794a3a630a9e830d7dba4abe90412f3
unknown <ZEAC122@live.rhul.ac.uk>
2019-03-26 14:11:33 +0000

    Fixed logins to make it work and redirect, going to add access tokens now

--- frontend/src/LoginComponents/LoginController.js
+++ frontend/src/LoginComponents/LoginController.js
@@ -9,40 +9,52 @@ import LoginPageWrapper from "../LoginComponents/LoginPageWrapper";
 
 export default class LoginController extends React.Component {
     constructor(props) {
-        super (props);
+        super(props);
         this.postData = this.postData.bind(this);
-        
+        this.state = {
+            loggedIn: false
+        }
     }
 
-     postData(loginBody) {
-         console.log(loginBody);
-        try{
-        fetch("https://flask.team-project.crablab.co/authentication/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: loginBody,
-        }).then(res => {
-            if(res.status === 500){
-                console.log("Bad Login");
-
-            } else {
-                res.json();
-            }
-        })
-          .then(res => console.log(res))
+    postData(loginBody) {
+        try {
+            return fetch("https://flask.team-project.crablab.co/authentication/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: loginBody,
+            }).then(res => {
+                if (res.status === 500) {
+                    return {login: false}
+                } else {
+                    return res.json();
+                }
+            })
+                .then(res => {
+                    var login = res.login;
+                    if (login !== false) {
+                        this.setState({
+                            loggedIn: true
+                        })
+                        return login;
+                    } else {
+                        this.setState({
+                            loggedIn: false
+                        })
+                    }
+                })
         } catch (error) {
             console.log(error);
         }
-     }
+    }
 
-     render() {
-         return(
-             <div className="LoginForm">
-                <LoginPageWrapper postData={this.postData}/>
-             </div>
+    render() {
+        return (
+            <div className="LoginForm">
+                <LoginPageWrapper loggedIn={this.state.loggedIn} postData={this.postData} />
+            </div>
 
-         )
-     }
- }
+        )
+    }
+}
--- frontend/src/LoginComponents/LoginPageWrapper.js
+++ frontend/src/LoginComponents/LoginPageWrapper.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button, Form } from 'semantic-ui-react';
-import {Link} from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 
 /**
  * This 
@@ -10,97 +10,100 @@ export default class LoginPageWrapper extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      staffID : null,
-      password : null,
+      staffID: null,
+      password: null,
+      loginPath: null,
+      accessToken : null,
+      userID : null
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.loggedIn = false;
   }
 
-  handleChange(event){
+  handleChange(event) {
     var target = event.target;
     var value = target.value;
     var name = target.name;
     this.setState({
-      [name] : value
+      [name]: value
     });
   }
 
-  handleLogin(event) {
-    alert('Login Attempted: ' + this.state.value);
-    event.preventDefault();
-  }
+  // handleLogin(event) {
+  //   alert('Login Attempted: ' + this.state.value);
+  //   event.preventDefault();
+  // }
 
-  waiterLogin() {
-    console.log(this.state.staffID);
-    console.log(this.state.password);
-    if (this.state.staffID == "test" && this.state.password == "s3kr3tp4ssw0rd") {
+  async handleLogin(type) {
     try {
       var loginRequestBody = JSON.stringify({
-        username : this.state.staffID,
-        password : this.state.password,
+        username: this.state.staffID,
+        password: this.state.password,
+        key: "abc123",
+        secret: "def456"
+      });
+      await this.props.postData(loginRequestBody)
+      .then(async (login) => {
+        if(login){ // Not undefined
+          console.log(login);
+          console.log("Am running");
+          await this.setState({
+            loginPath : "/"+type,
+            accessToken : login.acces_token,
+            userID : login.userID
+          })
+        }
+      })
+    } catch (error) {
+      console.log("Something went wrong!" + error);
+    }
+  }
+
+  kitchenLogin() { // Not needed 
+    try {
+      var loginRequestBody = JSON.stringify({
+        username: this.state.staffID,
+        password: this.state.password,
         key: "abc123",
         secret: "def456"
       });
       this.props.postData(loginRequestBody);
-      return true;
     } catch (error) {
       console.log("Something went wrong!" + error);
     }
-    } else {
-      // if details are not correct keep in login page and show error
-    }
+
   }
 
-  kitchenLogin() {
-    console.log(this.state.staffID);
-    console.log(this.state.password);
-    if (this.state.staffID == "kitchen" && this.state.password == "s3kr3tp4ssw0rd") {
-    try {
-      var loginRequestBody = JSON.stringify({
-        username : this.state.staffID,
-        password : this.state.password,
-        key: "abc123",
-        secret: "def456"
-      });
-      this.props.postData(loginRequestBody);
-    } catch (error) {
-      console.log("Something went wrong!" + error);
-    }
-    } else {
-      // if details are not correct keep in login page and show error
-    }
-  }
+  render() {
+    return (
+      <Form>
+        <Form.Field>
+          <label>Staff ID</label>
+          <input
+            placeholder='Please enter Staff ID here...'
+            name="staffID"
+            required onChange={this.handleChange}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Password</label>
+          <input placeholder='Please enter password here...'
+            name="password"
+            type = "password"
+            required onChange={this.handleChange}
+          />
+        </Form.Field>
+        {(this.props.loggedIn === true && this.state.loginPath !== null) &&
+         <Redirect to={{
+          pathname: this.state.loginPath
+        }}>
+        </Redirect>
+        }
+        <Button type='submit' onClick={() => { this.handleLogin("waiter") }}>Waiter Login</Button>
+        <Button type='submit' onClick={() => { this.handleLogin("kitchen") }}>Kitchen Login</Button>
+      </Form>
 
-    render() {
-        return(
-            <Form>
-                <Form.Field>
-                  <label>Staff ID</label>
-                  <input
-                   placeholder='Please enter Staff ID here...'
-                   name="staffID"
-                   required onChange={this.handleChange}
-                   />
-                </Form.Field>
-                <Form.Field>
-                  <label>Password</label>
-                  <input placeholder='Please enter password here...'
-                   name="password"
-                   required onChange={this.handleChange}
-                   />
-                </Form.Field>
-                <Link to={{
-                        pathname:"/waiter"
-                    }}>
-                <Button type='submit' onClick={()=>{this.waiterLogin()}}>Waiter Login</Button>
-                </Link>
-                <Link to={{
-                        pathname:"/Kitchen"
-                    }}>
-                <Button type='submit' onClick={()=>{this.kitchenLogin()}}>Kitchen Login</Button>
-                </Link>
-            </Form>
-            
-        )
-    }
+    )
+  }
 }
