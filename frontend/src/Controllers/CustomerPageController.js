5bb0d4b49da762c917acd4a2dc88cf326e7b250d
WilliamBr25 <ZEAC039@live.rhul.ac.uk>
2019-03-27 03:38:55 +0000

    removed test alert

--- frontend/src/Controllers/CustomerPageController.js
+++ frontend/src/Controllers/CustomerPageController.js
@@ -136,8 +136,6 @@ export default class CustomerPageController extends React.Component {
         }
 
         document.cookie = "orders=" + tempCookie;
-
-        alert(document.cookie);
     }
 
     render() {
